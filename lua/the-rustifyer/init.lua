95870d1b98ed5d77b85d68b27608b42b4cea788e
Alex Vergara <alex.vergara.dev@gmail.com>
2024-01-20 18:48:03 +0100

    feat(barebones): leader and Newtr remaps

---
+++ lua/the-rustifyer/init.lua
@@ -0,0 +1,5 @@
+-- The core of my Neovim perosnal setup
+--
+-- Here is where are defined the most important configuration steps
+
+require('remap')
---
+++ lua/the-rustifyer/remap.lua
@@ -0,0 +1,7 @@
+-- This file holds my personal keymaps and remaps, along as the configuration for the leader key
+--
+-- Not all remaps may be configured here, as they could be directly configured in the plugins
+
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
