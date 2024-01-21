8edd8b45b4f0c30acd5a9d4d26fff77a584425e3
Alex Vergara <alex.vergara.dev@gmail.com>
2024-01-21 18:42:55 +0100

    feat(telescope): added a custom remap to quickly open a theme selector

--- lua/the-rustifyer/config/remaps.lua
+++ lua/the-rustifyer/config/remaps.lua
@@ -2,9 +2,9 @@
 --
 -- Not all remaps may be configured here, as they could be directly configured in the plugins
 
-vim.g.mapleader = " "
+local noremap_silent = { noremap = true, silent = true };
 
--- Open netrw
+-- Opens `Newtr` file explorer 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- Telescope remaps
@@ -13,3 +13,5 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>cs', builtin.colorscheme, noremap_silent)
+
--- lua/the-rustifyer/init.lua
+++ lua/the-rustifyer/init.lua
@@ -2,9 +2,12 @@
 --
 -- Here is where are defined the most important configuration steps
 
--- First of all, we bring the most inner and important remaps
-require('the-rustifyer.config.remaps')
+-- Before all, let's assign our <leader> key
+vim.g.mapleader = " "
 
 -- Initialize the plugin manager and the plugins under <root_path>/.config/nvim/lua/plugins
 -- All of the plugins declared in such folder will be automatically detected and loaded by lazy
 require('the-rustifyer.config.lazy')
+
+-- Loading the custom remaps after loading the package manager
+require('the-rustifyer.config.remaps')
