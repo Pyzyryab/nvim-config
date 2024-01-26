fc2c4ed24b06aec12aac66a68ee1c03c3f989bae
= <=>
2024-01-26 10:06:14 +0100

    feat(telescope): fzf conditional installation and compilation depending on the underlying OS

--- lua/the-rustifyer/plugins/telescope-fzf-native.lua
+++ lua/the-rustifyer/plugins/telescope-fzf-native.lua
@@ -1,13 +1,16 @@
--- Telescope's native fzf
+-- Telescope's native fzf installation via lazy.nvim depending on
+-- the underlying OS detected by the lua jit
 --
--- Caution!: My setup is built with Cmake and Clang via the predefined
--- options described in the plugin's documentation.
---
--- In my case:
---	Linux: 	 Clang will be invoked natively, since I have a full LLVM
---		 configured setup in any of my Unix like distros always.
---
--- 	Windows: Clang will be invoked via the configured
--- 		 PATH to the `clang64` Msys2 environment.
---	 
-return { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+if jit then
+    local is_windows = jit.os == "Windows"
+    
+    if is_windows then
+        return { 'nvim-telescope/telescope-fzf-native.nvim', commit = "2330a7eac13f9147d6fe9ce955cb99b6c1a0face", optional = true }
+    else
+        return { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+    end
+else
+    print('Lua JIT not found. Trying default installation...')
+    return { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+end
+
