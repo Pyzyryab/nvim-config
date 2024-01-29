f6e3ef43420566f54283216bc72d9e62403ba58b
Alex Vergara <alex.vergara.dev@gmail.com>
2024-01-29 23:32:04 +0100

    feat(lsp): A little bit of fine tuning on the LSP configs at editor's start

--- lua/the-rustifyer/plugins/lsp/init.lua
+++ lua/the-rustifyer/plugins/lsp/init.lua
@@ -2,16 +2,16 @@
 --
 
 return {
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x', lazy = true, event = 'VeryLazy'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x', lazy = true, config = false,},
   {'williamboman/mason.nvim', lazy = true, event = 'VeryLazy'},
   {'williamboman/mason-lspconfig.nvim', lazy = true, event = 'VeryLazy'},
   {'neovim/nvim-lspconfig', lazy = true, event = 'VeryLazy'},
-  {'L3MON4D3/LuaSnip', lazy = true, event = 'BufEnter',},
-  {'hrsh7th/nvim-cmp', lazy = true, event = 'BufEnter',},
-  {'hrsh7th/cmp-nvim-lsp', lazy = true, event = 'BufEnter',},
-  {'hrsh7th/cmp-buffer', lazy = true, event = 'BufEnter',},
-  {'hrsh7th/cmp-path', lazy = true, event = 'BufEnter',},
-  {'saadparwaiz1/cmp_luasnip', lazy = true, event = 'BufEnter',},
-  {'rafamadriz/friendly-snippets', lazy = true, event = 'BufEnter',},
+  {'L3MON4D3/LuaSnip', lazy = true, event = {'BufReadPre', 'BufNewFile'},},
+  {'hrsh7th/nvim-cmp', lazy = true, event = 'InsertEnter',},
+  {'hrsh7th/cmp-nvim-lsp', lazy = true, event = {'BufReadPre', 'BufNewFile'},},
+  {'hrsh7th/cmp-buffer', lazy = true, event = {'BufReadPre', 'BufNewFile'},},
+  {'hrsh7th/cmp-path', lazy = true, event = {'BufReadPre', 'BufNewFile'},},
+  {'saadparwaiz1/cmp_luasnip', lazy = true, event = {'BufReadPre', 'BufNewFile'},},
+  {'rafamadriz/friendly-snippets', lazy = true, event = {'BufReadPre', 'BufNewFile'},},
 }
 
