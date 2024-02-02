e817f43786a28ff3ac78214a7134fda725135b80
Alex Vergara (The Rustifyer) <alex.vergara.dev@gmail.com>
2024-02-03 00:50:38 +0100

    feat(telescope): Added the telescope base configuration with the new setup

--- lua/the-rustifyer/plugins/specs/finders.lua
+++ lua/the-rustifyer/plugins/specs/finders.lua
@@ -3,7 +3,12 @@
 
 local plugins = {}
 
-plugins.telescope = 'nvim-telescope/telescope.nvim'
+plugins.telescope = {
+    'nvim-telescope/telescope.nvim',
+--    event = 'VeryLazy',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+}
 
 return plugins
 
