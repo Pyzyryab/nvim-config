1c78e1a9e64195fef63c553386c09988bc064df8
Alex Vergara <alex.vergara.dev@gmail.com>
2024-02-10 11:31:23 +0100

    (editor): mini.animate for common visual moments made now smooth

---
+++ lua/the-rustifyer/plugins/specs/editor.lua
@@ -0,0 +1,10 @@
+-- Plugins for enhace buffers when they can be seen or perceived
+-- as "editor" things
+return {
+    mini_animate = 'echasnovski/mini.animate',
+
+    -- Indentation guides
+    indent_blankline = 'lukas-reineke/indent-blankline.nvim',
+    mini_identscope = 'echasnovski/mini.indentscope',
+}
+
