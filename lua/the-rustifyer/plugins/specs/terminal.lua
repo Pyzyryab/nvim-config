664bf8ec44ebed3b62669da2785e4c8a74dc239d
= <=>
2024-02-29 20:59:22 +0100

    feat: Better nvim terminal

--- lua/the-rustifyer/plugins/specs/terminal.lua
+++ lua/the-rustifyer/plugins/specs/terminal.lua
@@ -1,5 +1,15 @@
 -- Plugins for having a terminal integrated within a Neovim session
 --
 return {
-    toggleterm = 'akinsho/toggleterm.nvim'
+    toggleterm = 'akinsho/toggleterm.nvim',
+    {
+        'gelguy/wilder.nvim',
+        event = 'VeryLazy',
+        config = function()
+            require('wilder').setup({
+                modes = { ':', '/', '?' }
+            })
+        end,
+        build = ':UpdateRemotePlugins'
+    },
 }
