0f1a0870d2286f744f62f91677e26bc7af6aa8bb
Alex Vergara <alex.vergara.dev@gmail.com>
2024-03-09 21:31:30 +0100

    feat: Better SmoothCursor config

--- lua/the-rustifyer/core/autocmds.lua
+++ lua/the-rustifyer/core/autocmds.lua
@@ -41,7 +41,7 @@ autocmd({ 'ModeChanged' }, {
     local current_mode = vim.fn.mode()
     if current_mode == 'n' then
       vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#8aa872' })
-      vim.fn.sign_define('smoothcursor', { text = '▷' })
+      vim.fn.sign_define('smoothcursor', { text = '󰝥' })
     elseif current_mode == 'v' then
       vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
       vim.fn.sign_define('smoothcursor', { text = '' })
--- lua/the-rustifyer/plugins/editor/general.lua
+++ lua/the-rustifyer/plugins/editor/general.lua
@@ -24,7 +24,7 @@ return {
     {
         'RRethy/vim-illuminate',
         enabled = false,
-        event = {'BufReadPre', 'BufNewFile'},
+        event = { 'BufReadPre', 'BufNewFile' },
         opts = {
             delay = 200,
             large_file_cutoff = 2000,
@@ -72,15 +72,43 @@ return {
         'gen740/SmoothCursor.nvim',
         event = 'VeryLazy',
         config = function()
-            vim.fn.sign_define('smoothcursor_v', { text = ' ' })
-            vim.fn.sign_define('smoothcursor_V', { text = '' })
-            vim.fn.sign_define('smoothcursor_i', { text = '' })
-            vim.fn.sign_define('smoothcursor_�', { text = '' })
-            vim.fn.sign_define('smoothcursor_R', { text = '󰊄' })
             require('smoothcursor').setup({
-                cursor = '▷',
-                fg = '#8aa872'
-                -- type = 'matrix'
+                type = "default", -- Cursor movement calculation method, choose "default", "exp" (exponential) or "matrix".
+
+                -- cursor = "", -- Cursor shape (requires Nerd Font). Disabled in fancy mode.
+                texthl = "SmoothCursor", -- Highlight group. Default is { bg = nil, fg = "#FFD400" }. Disabled in fancy mode.
+                linehl = nil, -- Highlights the line under the cursor, similar to 'cursorline'. "CursorLine" is recommended. Disabled in fancy mode.
+
+                fancy = {
+                    enable = true, -- enable fancy mode
+                    head = { cursor = "󰝥", texthl = "SmoothCursorAqua", linehl = nil }, -- false to disable fancy head
+                    body = {
+                        { cursor = "󰝥", texthl = "SmoothCursorAqua" },
+                        { cursor = "󰝥", texthl = "SmoothCursorAqua" },
+                        { cursor = "●", texthl = "SmoothCursorAqua" },
+                        { cursor = "●", texthl = "SmoothCursorAqua" },
+                        { cursor = "•", texthl = "SmoothCursorAqua" },
+                        { cursor = ".", texthl = "SmoothCursorAqua" },
+                        { cursor = ".", texthl = "SmoothCursorAqua" },
+                    },
+                    tail = { cursor = nil, texthl = "SmoothCursor" } -- false to disable fancy tail
+                },
+                autostart = true, -- Automatically start SmoothCursor
+                always_redraw = true, -- Redraw the screen on each update
+                flyin_effect = nil, -- Choose "bottom" or "top" for flying effect
+                speed = 25,    -- Max speed is 100 to stick with your current position
+                intervals = 35, -- Update intervals in milliseconds
+                priority = 10, -- Set marker priority
+                timeout = 3000, -- Timeout for animations in milliseconds
+                threshold = 1, -- Animate only if cursor moves more than this many lines
+                disable_float_win = false, -- Disable in floating windows
+                enabled_filetypes = nil, -- Enable only for specific file types, e.g., { "lua", "vim" }
+                disabled_filetypes = {'Alpha'}, -- Disable for these file types, ignored if enabled_filetypes is set. e.g., { "TelescopePrompt", "NvimTree" }
+                -- Show the position of the latest input mode positions.
+                -- A value of "enter" means the position will be updated when entering the mode.
+                -- A value of "leave" means the position will be updated when leaving the mode.
+                -- `nil` = disabled
+                show_last_positions = nil,
             })
         end
     },
@@ -100,4 +128,3 @@ return {
         end,
     },
 }
-
