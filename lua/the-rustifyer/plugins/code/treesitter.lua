3795f2a83d3d9b64161bcacf90824e5ffbe4197b
Alex Vergara <alex.vergara.dev@gmail.com>
2024-04-06 20:21:55 +0200

    feat: Added treesitter support for kdl language

--- lua/the-rustifyer/plugins/code/treesitter.lua
+++ lua/the-rustifyer/plugins/code/treesitter.lua
@@ -12,7 +12,7 @@ return {
         config = function()
             require('nvim-treesitter.configs').setup {
                 ensure_installed = { 'rust', 'cpp', 'c', 'cmake', 'make', 'python', 'java', 'go', 'lua', 'html',
-                    'sql', 'toml', 'yaml', 'llvm', 'gdscript', 'vim' },
+                    'sql', 'toml', 'yaml', 'llvm', 'gdscript', 'vim', 'kdl' },
                 -- Automatically install missing parsers when entering buffer
                 -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
                 auto_install = true,
