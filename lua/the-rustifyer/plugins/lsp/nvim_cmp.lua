e6d1b2e900c23805b45a67e0a0a71c573877fa50
Alex Vergara <alex.vergara.dev@gmail.com>
2024-12-13 10:39:47 +0100

    feat: upgraded de TAB and S_TAB actions for auto-completition

--- lua/the-rustifyer/plugins/lsp/nvim_cmp.lua
+++ lua/the-rustifyer/plugins/lsp/nvim_cmp.lua
@@ -36,6 +36,13 @@ return {
 
         -- LSP pictograms
         local lspkind = require('lspkind')
+        -- luasnip
+        local luasnip = require('luasnip')
+        -- Set this check up for nvim-cmp tab mapping
+        local has_words_before = function()
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
 
         cmp.setup({
             -- if you don't know what is a "source" in nvim-cmp read this:
@@ -66,6 +73,11 @@ return {
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
             },
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
             mapping = cmp.mapping.preset.insert({
                 -- confirm completion item
                 ['<CR>'] = cmp.mapping.confirm({ select = false }),
@@ -74,8 +86,27 @@ return {
                 ['<C-Space>'] = cmp.mapping.complete(),
 
                 -- Cycle the completition menu
-                ['<Tab>'] = cmp_action.luasnip_supertab(),
-                ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
 
                 -- scroll up and down the documentation window
                 ['<C-u>'] = cmp.mapping.scroll_docs(-4),
@@ -96,11 +127,6 @@ return {
                     show_labelDetails = true, -- show labelDetails in menu. Disabled by default
                 })
             },
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
-                end,
-            }
         })
 
         -- `/` cmdline setup.
